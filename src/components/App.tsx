f88a5519dfa76d46f92ce528d068def74f7d4d5f
Michann <michann.stoner@slalom.com>
2023-02-15 11:58:01 -0700

    Initial app setup

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { RequestLocation } from './Location/RequestLocation';
+
+
+function App() {
+  return (
+        <RequestLocation />
+    // <div>
+    //   <div>App Works!
+    //   </div>
+    // </div>
+  )
+}
+
+export default App;
