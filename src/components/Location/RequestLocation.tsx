c0c25c469f2ccf894679a3045ed76ffc8895efcc
Michann <michann.stoner@slalom.com>
2023-02-15 11:59:11 -0700

    Created custom hook, reducer, request location page

---
+++ src/components/Location/RequestLocation.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import useGetLocation from "../../services/useGetLocation";
+
+export const RequestLocation = (): any => {
+  const { isLoading, location } = useGetLocation();
+  const [checked, setChecked] = useState(false);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (!location.isActive) return <div>Error</div>;
+  return (
+    <div>
+      <div>{location.name}</div>
+      <div>You are requesting access to {location.name}</div>
+      <label>
+        <input
+          type="checkbox"
+          onChange={() => setChecked(!checked)}
+          checked={checked}
+        ></input>
+      </label>
+      <button disabled={!checked}>Request Location</button>
+    </div>
+  );
+};
---
+++ src/models/Location.ts
@@ -0,0 +1,6 @@
+export interface Location {
+  id: string;
+  location: string;
+  name: string;
+  isActive: boolean;
+}
